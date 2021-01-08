3e64367fd724de80c57b8746bfa67f764659ea4c
Gustavo Arendt <gustavo.arendt@outlook.com>
2021-01-08 20:30:12 -0300

    updated social links

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { ReactComponent as YoutubeIcon } from './youtube.svg'
 import { ReactComponent as LinkedinIcon } from './linkedin.svg'
-import { ReactComponent as InstagramIcon } from './instagram.svg'
+import { ReactComponent as GithubIcon } from './github.svg'
 import './styles.css';
 
 const Footer = () => {
@@ -8,14 +8,14 @@ const Footer = () => {
         <footer className="main-footer">
             App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
-                <a href="https://www.youtube.com/user/DevilGust" target="_new">
+                <a href="https://www.youtube.com/" target="_new">
                     <YoutubeIcon />
                 </a>
                 <a href="https://www.linkedin.com/in/gustavo-arendt-40417a139/" target="_new">
                     <LinkedinIcon />
                 </a>
-                <a href="https://www.instagram.com/gustyzero/" target="_new">
-                    <InstagramIcon />
+                <a href="https://github.com/gustavoarendt" target="_new">
+                    <GithubIcon />
                 </a>
             </div>
         </footer>
@@ -23,3 +23,4 @@ const Footer = () => {
 }
 
 export default Footer;
+
